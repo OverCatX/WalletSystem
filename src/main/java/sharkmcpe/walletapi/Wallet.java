25e9a166fc3244a6fe998a1fa58e9e49b68a2ff0
OverCatX <74818019+OverCatX@users.noreply.github.com>
2020-12-19 13:11:09 +0700

    Update Wallet.java

--- src/main/java/sharkmcpe/walletapi/Wallet.java
+++ src/main/java/sharkmcpe/walletapi/Wallet.java
@@ -4,7 +4,16 @@ import cn.nukkit.event.Listener;
 import cn.nukkit.plugin.PluginBase;
 
 public class Wallet extends PluginBase implements Listener {
-
+    
+    static Wallet wallet = null;
+    
+    public static Mail getMe(){
+      return wallet;
+    }
+    @Override
+    public void onLoad(){
+        wallet = this;
+    }
     @Override
     public void onEnable() {
         getLogger().info("สร้างโดย SharkMCPE");
